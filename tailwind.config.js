b15815aa309ecc0a9143f9128d918aabfed250b4
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-11-15 19:18:34 +0530

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#010c4f",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+      },
+      fontFamily: {
+      },
+    },
+  },
+  plugins: [],
+}
+
